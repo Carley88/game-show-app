3a6ee40e2b4c9983470ad0a66eb70fdb5ba83a83
Carley Jeffrey <carleyjeffrey88@gmail.com>
2020-10-15 11:50:08 +0100

    Add game class with missed, phrases and activePhrases properties

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+  constructor(missed, phrases, activePhrase) {
+    this.missed = 0;
+    this.phrases = ["A Piece of Cake", "Roll With the Punches", "Break The Ice", "Close But No Cigar", "You Can't Judge a Book By Its Cover"];
+    this.activePhrase = null;
+  }
+}
