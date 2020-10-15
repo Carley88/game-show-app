62dbac667a843835516094a1a636f156bf37b319
Carley Jeffrey <carleyjeffrey88@gmail.com>
2020-10-15 15:21:29 +0100

    Add method to start the game

--- js/Game.js
+++ js/Game.js
@@ -12,4 +12,8 @@
   const randomNumber = Math.floor(Math.random() * 5);
   return this.phrases[randomNumber];
   }
+  startGame() {
+    const startScreen = document.getElementById("overlay");
+    startScreen.style.display = "none";
+  }
 }
