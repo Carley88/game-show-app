17e1fc2db15ffab3591cc8e3207bb39426f2930c
Carley Jeffrey <carleyjeffrey88@gmail.com>
2020-10-15 11:59:06 +0100

    Add method to get a random phrase from the phrases property

--- js/Game.js
+++ js/Game.js
@@ -8,4 +8,8 @@
     this.phrases = ["A Piece of Cake", "Roll With the Punches", "Break The Ice", "Close But No Cigar", "You Can't Judge a Book By Its Cover"];
     this.activePhrase = null;
   }
+  getRandomPhrase() {
+  const randomNumber = Math.floor(Math.random() * 5);
+  return this.phrases[randomNumber];
+  }
 }
